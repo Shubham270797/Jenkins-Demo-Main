c6d542f37778f2a0bceb6d87a1ddd3a5b11fb950
amdcdac02 <amdcdac02@amdcdac09.iiht.tech>
2022-06-16 11:16:08 +0530

    Demo main

---
+++ src/main/java/com/training/amdocs/JenkinsDemoMain/JenkinsDemoMainApplication.java
@@ -0,0 +1,13 @@
+package com.training.amdocs.JenkinsDemoMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/amdocs/JenkinsDemoMain/JenkinsDemoMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.amdocs.JenkinsDemoMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
